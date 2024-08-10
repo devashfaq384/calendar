ef0071efe9b948c04a8ed657a87de90cefabd075
devashfaq384 <98392313+devashfaq384@users.noreply.github.com>
2024-08-11 00:55:03 +0500

    Calendar complete

--- src/components/Activity/Recent.jsx
+++ src/components/Activity/Recent.jsx
@@ -2,7 +2,7 @@ import RecentItem from "./_components/RecentItem"
 
 const Recent = () => {
     return (
-      <div className="bg-white rounded-[12px] mt-[21px]md:ml-0 ml-[21px] mr-[21px] border-[1px] border-[border_color] p-[20px]" >
+      <div className="bg-white rounded-[12px] mt-[21px] md:ml-0 ml-[21px] mr-[21px] border-[1px] border-[border_color] p-[20px]" >
           <div className="text-[14px] font-[600]" >Recent Activity</div>
           {/* items info  */}
           <div>
